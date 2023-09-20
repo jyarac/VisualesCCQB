6abe1de303ae213de91e852dcb8f999f7597b603
Johhan Stiven Maldonado Benavides <80873196+jomaldonadob@users.noreply.github.com>
2023-09-19 20:22:45 -0500

    Arbol

--- Modelacion de arbol/sketch.js
+++ Modelacion de arbol/sketch.js
@@ -1,26 +1,40 @@
 function setup() {
   createCanvas(680, 400, WEBGL);
+  angleMode(DEGREES); // Cambia el modo de ángulo a grados
 }
 
-let i = 0;
+let angle = 0; // Variable para controlar el ángulo de crecimiento
 
 function draw() {
   background(0);
-  //camera(0, 400, 400);
   orbitControl();
   
+  translate(0, height / 2); // Alinea el centro del tronco con la mitad de la altura
+  
+  // Llama a una función que dibuja la estructura
+  drawPlant(100, 10); // Los valores son solo ejemplos
+  
   axes();
+}
 
-  translate(25, -25, 0)
-  
-  translate(-25, 25, 0)
-  rotateZ(QUARTER_PI)
+function drawPlant(len, level) {
+  if (level > 0) {
+    // Dibuja un tronco
+    stroke(139, 69, 19); // Color marrón
+    strokeWeight(2);
+    line(0, 0, 0, 0, -len, 0); // Dibuja el tronco hacia abajo
 
-  scale(1.0)
-  translate(-25, -25, 0)
-  
-  box(50);
+    translate(0, -len); // Mueve el punto de origen al extremo del tronco
+    push(); // Guarda la transformación actual
+    rotate(angle); // Rota a la derecha
+    drawPlant(len * 0.67, level - 1); // Dibuja una rama derecha recursivamente
+    pop(); // Restaura la transformación anterior
 
+    push(); // Guarda la transformación actual
+    rotate(-angle); // Rota a la izquierda
+    drawPlant(len * 0.67, level - 1); // Dibuja una rama izquierda recursivamente
+    pop(); // Restaura la transformación anterior
+  }
 }
 
 function axes() {
