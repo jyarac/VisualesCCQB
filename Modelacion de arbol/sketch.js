b18c62b9fc7fdbd52f78310ce94dc7ec39ea9cd6
Johhan Stiven Maldonado Benavides <80873196+jomaldonadob@users.noreply.github.com>
2023-09-19 20:16:03 -0500

    Eso tilin

--- Modelacion de arbol/sketch.js
+++ Modelacion de arbol/sketch.js
@@ -1,7 +1,38 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(680, 400, WEBGL);
 }
 
+let i = 0;
+
 function draw() {
-  background(220);
+  background(0);
+  //camera(0, 400, 400);
+  orbitControl();
+  
+  axes();
+
+  translate(25, -25, 0)
+  
+  translate(-25, 25, 0)
+  rotateZ(QUARTER_PI)
+
+  scale(1.0)
+  translate(-25, -25, 0)
+  
+  box(50);
+
+}
+
+function axes() {
+  strokeWeight(4);
+  stroke(255, 0, 0);
+  line(0, 0, 0, 100, 0, 0);
+
+  strokeWeight(4);
+  stroke(0, 255, 0);
+  line(0, 0, 0, 0, 100, 0);
+
+  strokeWeight(4);
+  stroke(0, 0, 255);
+  line(0, 0, 0, 0, 0, 100);
 }
