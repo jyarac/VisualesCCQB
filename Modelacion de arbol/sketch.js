e1d50c00a6a463cd64f0a545df78a0c486e93342
Johhan Stiven Maldonado Benavides <80873196+jomaldonadob@users.noreply.github.com>
2023-09-19 20:29:00 -0500

    3.0v

--- Modelacion de arbol/sketch.js
+++ Modelacion de arbol/sketch.js
@@ -1,38 +1,49 @@
+let treeDepth = 5; // Profundidad del árbol
+let flowerSize = 50; // Tamaño de la flor
+let grassCount = 100; // Cantidad de tallos de pasto
+let grassMaxHeight = 100; // Altura máxima del pasto
+
 function setup() {
-  createCanvas(680, 400, WEBGL);
+  createCanvas(800, 600, WEBGL);
   angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
   orbitControl();
-  
+
   // Configuración 1: Árbol
-  translate(-100, 0, 0);
-  drawTree(100, 5);
-  
+  push();
+  translate(-150, 0, 0);
+  drawTree(100, treeDepth);
+  pop();
+
   // Configuración 2: Flor
-  translate(200, 0, 0);
-  drawFlower(50, 8);
-  
-  // Configuración 3: Hierba
-  translate(200, 0, 0);
-  drawGrass(10, 100);
-  
+  push();
+  translate(0, 0, 0);
+  drawFlower(flowerSize, 8);
+  pop();
+
+  // Configuración 3: Pasto
+  push();
+  translate(150, 0, 0);
+  drawGrass(grassCount, grassMaxHeight);
+  pop();
+
   axes();
 }
 
-function drawTree(len, level) {
-  if (level > 0) {
+function drawTree(len, depth) {
+  if (depth > 0) {
     stroke(139, 69, 19); // Marrón
     strokeWeight(2);
     line(0, 0, 0, 0, -len, 0); // Tronco
-    
+
     translate(0, -len);
     for (let i = 0; i < 3; i++) {
       push();
       rotate(random(-30, 30));
-      drawTree(len * 0.67, level - 1);
+      drawTree(len * random(0.6, 0.8), depth - 1);
       pop();
     }
   }
@@ -42,7 +53,7 @@ function drawFlower(radius, petals) {
   noStroke();
   fill(255, 0, 0); // Rojo
   sphere(radius);
-  
+
   for (let i = 0; i < petals; i++) {
     push();
     rotate(360 / petals * i);
@@ -56,14 +67,14 @@ function drawFlower(radius, petals) {
 function drawGrass(count, maxHeight) {
   noStroke();
   fill(0, 255, 0); // Verde
-  
+
   for (let i = 0; i < count; i++) {
     let x = random(-width / 2, width / 2);
     let y = random(-height / 2, height / 2);
     let h = random(20, maxHeight);
-    
+
     push();
-    translate(x, y, 0);
+    translate(x, y, h / 2);
     box(5, 5, h);
     pop();
   }
