cd84ea7f108a6fcfc1d8bd2c093a3a407702dec2
jyarac <jyarac@unal.edu.co>
2023-09-22 18:21:49 -0500

    opcion 2 con las 3 configuraciones

---
+++ Arbol recursivo/opicion2.js
@@ -0,0 +1,113 @@
+// Daniel Shiffman
+// Code for: https://youtu.be/E1B4UoSQMFw
+
+// variables: A B
+// axiom: A
+// rules1: (A → AB), (B → A)
+var angle;
+var axiom = "F";
+var sentence = axiom;
+
+var len = 180;
+
+var rules1 = [];
+rules1[0] = {
+  a: "F",
+  b: "F/[+F-F'F]-[-F/F+F]"
+}
+//hagan la configuraion que quieran con las siguientes reglas
+var rules2 = [];
+rules2[0] = {
+  a: "F",
+  b: "FF/[+F-F/F]-[-F+X+F]/[F]"
+}
+rules2[1] = {
+    a: "X",
+    b: "/F[+X]F['X]+X"
+}
+var rules3 = [];
+rules3[0] = {
+  a: "F",
+  b: "FF/[+F-F-F]'[-F+X-F]"
+}
+function draw(){
+    drawTree();
+    orbitControl();
+}
+
+function drawTree() {
+    background(51);
+    resetMatrix();
+    translate(width/50, height/2);
+    stroke(255, 100);
+    for (var i = 0; i < sentence.length; i++) {
+      var current = sentence.charAt(i);
+      if (current == "F") {
+        line(0, 0, 0, -len);
+        translate(0, -len);
+      } else if (current == "+") {
+        rotate(angle);
+      } else if (current == "-") {
+        rotate(-angle)
+      } else if (current == "[") {
+        push();
+      } else if (current == "]") {
+        pop();
+      } else if (current == "X") {
+        line(0, 0, 0, -len/2);
+        translate(0, -len/2);
+      } else if (current == "/") { 
+        rotateY(angle);
+      } else if (current == "'") {
+        rotateY(-angle);
+      }
+  }
+}
+
+//para usar un conjutno diferente de reglas cambien esta funcion
+function generate(actualRules) {
+  len *= 0.5;
+  var nextSentence = "";
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+    var found = false;
+    for (var j = 0; j < actualRules.length; j++) {
+      if (current == actualRules[j].a) {
+        found = true;
+        nextSentence += actualRules[j].b;
+        break;
+      }
+    }
+    if (!found) {
+      nextSentence += current;
+    } 
+  }
+  sentence = nextSentence;
+}
+function setup() {
+    createCanvas(windowWidth, windowHeight/1.2, WEBGL); 
+    angle = radians(25);
+    background(51);
+    var button1 = createButton("Crear siguiente generacion configuracion 1");
+    var button2 = createButton("Crear siguiente generacion configuracion 2");
+    var button3 = createButton("Crear siguiente generacion configuracion 3");
+    var restartButton = createButton("Reiniciar configuracion");
+    button1.mousePressed(useRules1);
+    button2.mousePressed(useRules2);
+    button3.mousePressed(useRules3);
+    restartButton.mousePressed(restart);
+  }
+function restart(){
+    len = 100;
+    sentence = axiom;
+}
+
+function useRules1(){
+    generate(rules1);
+}
+function useRules2(){
+    generate(rules2);
+}
+function useRules3(){
+    generate(rules3);
+}
--- Arbol recursivo/sketch.js
+++ Arbol recursivo/sketch.js
@@ -1,6 +1,7 @@
 let barkTexture;
 let leafTexture;
 let growthFactor = 0.5; // Factor de crecimiento del árbol.
+let maxHeight = 30; // Altura máxima del árbol. 
 
 function preload() {
   barkTexture = loadImage('corteza.png'); 
@@ -10,12 +11,17 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
+  
 }
 
 function draw() {
+  orbitControl();
   background(0);
   translate(0,height/2);  // Centramos el árbol verticalmente
+  if (10 + growthFactor * frameCount < maxHeight) {
   branch(10 + growthFactor * frameCount); // La longitud de la rama inicial aumenta con el tiempo.
+  console.log(10 + growthFactor * frameCount);
+  }
 }
 
 function branch(len) {
@@ -25,36 +31,38 @@ function branch(len) {
   line(0, 0, 0, 0, -len - 2, 0);
   translate(0, -len, 0);
 
-  if (len > 10) {
-    for (var i = 0; i < 3; i++) {
-      rotateY(random(10, 140));
-      push();
-      rotateZ(random(20, 50));
-      branch(len * 0.8); 
-      pop();
-    }
-  } else {
-    var r = 80 + random(-20,20);
-    var g = 120+ random(-20,20);
-    var b = 40+ random(-20,20);
+    if (len > 10) {
+      for (var i = 0; i < 3; i++) {
+        rotateY(random(10, 140));
+        push();
+        rotateZ(random(20, 50));
+        branch(len * 0.8); 
+        pop();
+      }
+    } else {
+      var r = 80 + random(-20,20);
+      var g = 120+ random(-20,20);
+      var b = 40+ random(-20,20);
 
-    fill(r,g,b,200);
-    noStroke();
-    translate(5, 0, 0);
-    rotateZ(90);
-    beginShape();
-    for (var i = 45; i < 135; i++) {
-      var rad = 7;
-      var x = rad * cos(i);
-      var y = rad * sin(i);
-      vertex(x, y);
-    }
-    for (var i = 135; i > 45; i--) {
-      var rad = 7;
-      var x = rad * cos(i);
-      var y = rad * sin(-i) + 20;
-      vertex(x, y);
-    }
-    endShape(CLOSE);
+      fill(r,g,b,200);
+      noStroke();
+      translate(5, 0, 0);
+      rotateZ(90);
+      beginShape();
+
+      for (var i = 45; i < 135; i++) {
+        var rad = 7;
+        var x = rad * cos(i);
+        var y = rad * sin(i);
+        vertex(x, y);
+      }
+      for (var i = 135; i > 45; i--) {
+        var rad = 7;
+        var x = rad * cos(i);
+        var y = rad * sin(-i) + 20;
+        vertex(x, y);
+      }
+      endShape(CLOSE);
+      }
   }
-}
+
