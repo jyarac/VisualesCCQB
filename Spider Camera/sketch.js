135f074e9893c8277be923455399402c65f52e9e
dabonilla <dabonilla@unal.edu.co>
2023-10-07 21:36:12 -0500

    Añadiendo espacio entre los edificos y colores.

--- Spider Camera/sketch.js
+++ Spider Camera/sketch.js
@@ -1,6 +1,10 @@
-let citySize = 10; 
+let citySize = 8; 
 let blockSize = 50;
+let buildingSpacing = 100; // Espacio entre los edificios
 let city = [];
+let buildingColors = [];
+let path = [];
+let index = 0;
 
 function setup() {
     createCanvas(800, 600, WEBGL);
@@ -8,23 +12,31 @@ function setup() {
         city[i] = [];
         for (let j = 0; j < citySize; j++) {
             city[i][j] = random(50, 200);
+            buildingColors.push(color(random(255), random(255), random(255)));
         }
     }
 }
 
 function draw() {
     background(220);
-    orbitControl();  // Allows you to rotate the view using the mouse
+    orbitControl();  // Permite rotar la vista con el ratón
+    
     drawCity();
 }
 
 function drawCity() {
+    index = 0
     for (let i = 0; i < citySize; i++) {
+        let randomColor = buildingColors[index];
         for (let j = 0; j < citySize; j++) {
             push();
-            translate(i * blockSize - citySize * blockSize / 2, 0, j * blockSize - citySize * blockSize / 2);
+            translate(i * (blockSize + buildingSpacing) - (citySize - 1) * (blockSize + buildingSpacing) / 2, 0, j * (blockSize + buildingSpacing) - (citySize - 1) * (blockSize + buildingSpacing) / 2);
+            fill(randomColor)
             box(blockSize, city[i][j], blockSize);
             pop();
+            index++;
         }
     }
+
+    
 }
